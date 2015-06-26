218e5a7c0106378139aff13b55b455c863019ff1
Josbourne2 <jos.menhart@live.nl>
2015-06-26 17:29:15 +0200

    First version

---
+++ Popup Dictionairy/DictionairyForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Popup_Dictionairy
+{
+    public partial class DictionairyForm : Form
+    {
+        int interval;
+
+        public DictionairyForm()
+        {
+            interval = SettingsManager.Current.QuestionIntervalSeconds;            
+            InitializeComponent();
+        }
+
+        private void DictionairyForm_Resize(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Popup Dictionairy";
+            notifyIcon1.BalloonTipText = String.Format("The program will popup every {0} seconds. Click here if you want to be asked extra questions.",interval.ToString());
+
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+       
+    }
+}
---
+++ Popup Dictionairy/SettingsManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace Popup_Dictionairy
+{
+    class SettingsManager
+    {
+        private static  SettingsManager _instance;
+
+        
+
+        private SettingsManager()
+        {
+
+        }
+
+        public static SettingsManager Current 
+        { 
+            get 
+            {
+                return _instance??(_instance = new SettingsManager());
+            }        
+        
+        }
+
+        public int QuestionIntervalSeconds 
+        { 
+            get
+            {
+                return Properties.Settings.Default.QuestionIntervalSeconds;
+            }
+        }
+
+        private void DisplayMessage(string message)
+        {
+            MessageBox.Show("The following parameter may not be defined: {0}", message);
+        }
+    }
+}
