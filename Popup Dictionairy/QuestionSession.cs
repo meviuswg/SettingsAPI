d1097eb4ddd4e69c874190798bbb6d2f816fd339
meviuswg <wouter.mevius@outlook.com>
2015-06-28 18:54:31 +0200

    QuestionSession controller

---
+++ Popup Dictionairy/QuestionSession.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Popup_Dictionairy
+{
+    internal class QuestionSession
+    {
+        private Translation[] questionList;
+        private int size;
+        private int currentTranslationIndex = 0;
+
+        public QuestionSession()
+            : this(10)
+        {
+        }
+
+        public QuestionSession(int numberOfQuestions)
+        {
+            size = numberOfQuestions;
+        }
+
+        public Translation Next()
+        {
+            currentTranslationIndex++;
+
+            if (currentTranslationIndex > questionList.Length - 1)
+                return null;
+
+            var translation = questionList[currentTranslationIndex];
+            return translation;
+        }
+
+        private void Init()
+        {
+            questionList = (from translation in TranslationProvider.Instance.Translations
+                            select translation).Take(size).ToArray();
+        }
+    }
+}
