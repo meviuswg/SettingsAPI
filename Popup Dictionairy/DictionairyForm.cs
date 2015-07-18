5220546cd626716fe769704eb570097866a304d2
Josbourne2 <jos.menhart@live.nl>
2015-07-18 23:09:04 +0200

    Repaired balloontiptext

--- Popup Dictionairy/DictionairyForm.cs
+++ Popup Dictionairy/DictionairyForm.cs
@@ -26,15 +26,15 @@ namespace Popup_Dictionairy
             InitializeComponent();
             notifyIcon1.BalloonTipClicked += notifyIcon1_BalloonTipClicked;
             notifyIcon1.BalloonTipTitle = "Popup Dictionairy";
-            notifyIcon1.BalloonTipText = String.Format("The program will popup every {0} seconds. Click here if you want to change the interval.", (interval).ToString());
+            notifyIcon1.BalloonTipText = String.Format("The program will popup every {0} seconds. Click here if you want to change the interval.", (interval / 1000).ToString());
         }
 
         private void Current_QuestionIntervalChanged(object sender, int e)
         {
             timer1.Stop();
             timer1.Interval = e;
-            interval = e / 1000;
-            notifyIcon1.BalloonTipText = String.Format("The program will popup every {0} seconds. Click here if you want to change the interval.", (interval).ToString());
+            interval = e;
+            notifyIcon1.BalloonTipText = String.Format("The program will popup every {0} seconds. Click here if you want to change the interval.", (interval / 1000).ToString());
             timer1.Start();
         }
 
