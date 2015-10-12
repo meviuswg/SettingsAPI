48fc7b5e3a9198293c1be70576232335dfb1d78d
mevuiswg <wouter.mevius@gmail.com>
2015-10-12 22:48:20 +0200

    Constants

---
+++ SettingsAPI/SettingsAPIData/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsAPIData
+{
+    public class Log
+    {
+        private static Action<string> _logger;
+
+        public static Action<string> Logger { get { return _logger; } set { _logger = value; } }
+
+        public static void Message(string message)
+        {
+            if(_logger != null)
+            {
+                _logger(message);
+            }
+        }
+
+        public static void Exception(Exception ex)
+        {
+            Message(ex.ToString());
+        }
+    }
+}
---
+++ SettingsAPI/SettingsAPIData/Model/ApiKeyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsAPIData.Model
+{
+    public class ApiKeyModel
+    {
+        public ApiKeyModel()
+        {
+            Access = new List<ApiAccessModel>();
+        }
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public bool Active { get; set; }
+        public bool AdminKey { get; set; }
+        public List<ApiAccessModel> Access;
+    }
+}
